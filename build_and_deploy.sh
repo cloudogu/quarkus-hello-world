7d8f8c01a95da993d4e93dcb83e321238759f2b8
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2023-03-27 14:36:16 +0200

    add build script that will auth to the image registry and build, push and deploy the image

---
+++ build_and_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+registry=eu.gcr.io
+
+# authenticate to Image Registry
+gcloud auth login --cred-file ~/svcacc.json
+#gcloud auth print-access-token | podman login -u oauth2accesstoken $registry
+gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://$registry
+
+# build, push and deploy
+./gradlew clean build \
+    -Dquarkus.container-image.build=true \
+    -Dquarkus.container-image.push=true \
+    -Dquarkus.kubernetes.deploy=true
+
+#podman push eu.gcr.io/ces-operations-internal/quarkus-hello-world:1.0.0-SNAPSHOT
