3b226d50692bfed706a5a9e147d7b79a5aa33799
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2023-03-29 13:43:28 +0200

    add build_and_deploy script for reference

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 registry=eu.gcr.io
-
 # authenticate to Image Registry
 gcloud auth login --cred-file ~/svcacc.json
 #gcloud auth print-access-token | podman login -u oauth2accesstoken $registry
@@ -10,6 +9,8 @@ gcloud auth print-access-token | docker login -u oauth2accesstoken --password-st
 ./gradlew clean build \
     -Dquarkus.container-image.build=true \
     -Dquarkus.container-image.push=true \
-    -Dquarkus.kubernetes.deploy=true
+    -Dquarkus.kubernetes.deploy=true \
+    -Dquarkus.kubernetes.namespace="${CODER_CLOUDOGU_DEPLOY_NAMESPACE}"
+
 
 #podman push eu.gcr.io/ces-operations-internal/quarkus-hello-world:1.0.0-SNAPSHOT
