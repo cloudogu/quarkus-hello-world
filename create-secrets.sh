6efb0565e9226b8d4bbe743eaeef9b1acb2e825e
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2023-03-29 16:07:40 +0200

    remove script for secrets as the command for it is in the readme

--- create-secrets.sh
+++
@@ -1,5 +0,0 @@
-kubectl create secret generic postgres-credentials \
-    --from-literal=db=postgres \
-    --from-file=username=./postgres_username.txt \
-    --from-file=password=./postgres_password.txt \
-    -n coder-ws-3d1b8534-7cbf-4f02-a8fb-c9191ca58e3b
